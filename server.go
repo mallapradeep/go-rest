00f596d40ff5d315f72d930f4b51f238252d6d88
pradeepmalla <mallapradeep88@gmail.com>
2019-10-11 12:46:41 -0500

    REST Ready

--- server.go
+++ server.go
@@ -2,23 +2,69 @@ package main
 
 import (
 	// Standard library packages
-	"fmt"
+
 	"net/http"
 
 	// Third party packages
 	"github.com/julienschmidt/httprouter"
+	"github.com/mallapradeep.go-rest/controllers"
 )
 
 func main() {
 	// Instantiate a new router
 	r := httprouter.New()
 
-	// Add a handler on /test
-	r.GET("/test", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		// Simply write some test data for now
-		fmt.Fprint(w, "Welcome!\n")
-	})
+	// Get a UserController instance
+	uc := controllers.NewUserController
+
+	// Get a user resource
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.RemoveUser)
 
 	// Fire up the server
 	http.ListenAndServe("localhost:3000", r)
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+	// // Add a handler on /test
+	// // Get a user resource
+	// r.GET("/user/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// 	// Stub an example user
+	// 	u := models.User{
+	// 		Name:   "Pradeep Malla",
+	// 		Gender: "male",
+	// 		Age:    50,
+	// 		Id:     p.ByName("id"),
+	// 	}
+
+	// 	// Marshal provided Interface to JSON structure
+	// 	uj, _ := json.Marshal(u)
+
+	// 	// Write content-type, statuscode, payload
+	// 	w.Header().Set("Content-type", "application/json")
+	// 	w.WriteHeader(200)
+	// 	fmt.Fprintf(w, "%s", uj)
+	// })
+
+	// r.POST("/user", func(w http.ResponseWriter, r *http.Request, _httprouter.Params){
+	// 	// Stub an user to be populated from the body
+	// 	u := models.User{}
+
+	// 	// Populate the user data
+	// 	json.NewDecoder(r.Body).Decode(&u)
+
+	// 	// Add an Id
+	// 	u.Id = "foo"
+
+	// 	// Write content-type, status-code, payload
+	// 	w.Header().Set("Content-Type", "application/json")
+	// 	w.WriteHeader(201)
+	// 	fmt.Fprintf(w, "%s", uj)
+	// })
+
+	// r.DELETE("/user/:id", func(w http.ResponseWriter, r *http.Request, _httprouter.Params){
+	// 	// on;y write status for now
+	// 	w.WriteHeader(200)
+	// })
+
 }
